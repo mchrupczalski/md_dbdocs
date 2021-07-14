094c9c951f46368ed063f59c33cd029dbd4ff935
Mateusz Chrupczalski <m.chrupczalski@outlook.com>
2021-07-14 10:34:52 +0100

    moving logic to lib and rework flow

---
+++ md_dbdocs.lib/Serializers/JsonSerializer.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Globalization;
+
+namespace md_dbdocs.lib.Serializers
+{
+    public static class JsonSerializer
+    {
+        /// <summary>
+        /// Deserialize JSON to C# class object
+        /// </summary>
+        /// <typeparam name="T">Target class</typeparam>
+        /// <param name="jsonString">JSON string</param>
+        /// <returns></returns>
+        public static T FromJson<T>(string jsonString) => JsonConvert.DeserializeObject<T>(jsonString, Settings);
+
+        /// <summary>
+        /// Serialize C# class to JSON
+        /// </summary>
+        /// <typeparam name="T">Generic type</typeparam>
+        /// <param name="model">Class object to serialize</param>
+        /// <returns>JSON string</returns>
+        public static string ToJson<T>(T model) => JsonConvert.SerializeObject(model, Settings);
+
+        /// <summary>
+        /// Serializers settings
+        /// </summary>
+        private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
---
+++ md_dbdocs.lib/Serializers/YamlSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using YamlDotNet.Serialization;
+
+namespace md_dbdocs.lib.Serializers
+{
+    public static class YamlSerializer
+    {
+        /// <summary>
+        /// Serialize C# Class to YAML
+        /// </summary>
+        /// <typeparam name="T">Generic Type</typeparam>
+        /// <param name="model">Class object to serialize</param>
+        /// <returns>YAML string</returns>
+        public static string Serialize<T>(T model)
+        {
+            var serializer = new SerializerBuilder().Build();
+            return serializer.Serialize(model);
+        }
+
+        /// <summary>
+        /// Deserialize YAML to C# class object
+        /// </summary>
+        /// <typeparam name="T">Target class</typeparam>
+        /// <param name="yaml">YAML string</param>
+        /// <returns></returns>
+        public static T DeSerialize<T>(string yaml)
+        {
+            var deserializer = new DeserializerBuilder().Build();
+            return deserializer.Deserialize<T>(yaml);
+        }
+    }
+}
